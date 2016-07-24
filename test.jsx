8db228cb15b0227a8346279ec781158b36af237f
miaoihan <izhanghan@outlook.com>
2016-07-24 22:48:44 +0800

    finish all demo

--- test.jsx
+++ test.jsx
@@ -1,1 +1,24 @@
-test.jsx
+class MyTitle extends React.Component{
+    	propTypes: {
+    		title: React.PropTypes.string.isRequired
+  	}
+  	getDefaultPros(){
+  		return{
+  			title: 'this is default title'
+  		}
+  	}
+
+    	render(){
+    		// return <h1>{ this.props.name }</h1>
+    		return <h1>{ this.props.title }</h1>
+    	}
+    }
+
+    	let data = 123
+      ReactDOM.render(
+      	// <MyTitle name={data}></MyTitle>,
+      	<MyTitle/>,
+        example
+        )
+
+      
